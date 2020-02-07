0d8155c5f8adc110ed1c9101865d4209557e39a7
Rishabgh <rishabgh@Rishabghs-MacBook-Pro.local>
2020-02-07 18:15:26 +0530

    add axis and axis labels

--- public/js/buildings.js
+++ public/js/buildings.js
@@ -7,30 +7,65 @@ const drawChart = function(buildings) {
   const nameOfBuildings = buildings.map(building => building.name);
   const heightOfTallestBuildings = getMaxHeight(buildings);
   const chartAreaContainer = d3.select(htmlIDs.chartArea);
-  const svg = drawSvg(chartAreaContainer, svgHeight, svgWidth);
+  const svg = drawSvg(chartAreaContainer, chartHeight, chartWidth);
   const rectangles = svg.selectAll("rect").data(buildings);
   const newRectangels = rectangles.enter().append("rect");
+
+  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+  const width = chartWidth - (margin.left + margin.right);
+  const height = chartHeight - (margin.top + margin.bottom);
 
   const xScale = d3
     .scaleBand()
     .domain(nameOfBuildings)
-    .range([0, svgWidth])
+    .range([10, svgWidth])
     .padding(0.3);
 
   const yScale = d3
     .scaleLinear()
     .domain([0, heightOfTallestBuildings])
-    .range([0, 400]);
+    .range([0, 300]);
 
   newRectangels
     .attr("y", 0)
-    .attr("x", b => xScale(b.name))
+    .attr("x", b => xScale(b.name) + 100)
     .attr("width", xScale.bandwidth)
     .attr("height", b => yScale(b.height))
     .attr("fill", "grey");
 
-  const buildingsG = svg.append("g");
-  buildingsG.selectAll()
+  const buildingsG = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left} ${margin.top})`);
+
+  buildingsG
+    .append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 3)
+    .attr("y", height + 140)
+    .text("tall buildings");
+
+  const yAxis = d3.axisLeft(yScale);
+  const xAxis = d3.axisBottom(xScale);
+
+  buildingsG
+    .append("text")
+    .attr("class", "y axis-label")
+    .attr("x", -150)
+    .attr("y", -40)
+    .attr("transform", `rotate (-90)`)
+    .text("height (M)");
+
+  buildingsG.append("g").call(yAxis);
+  buildingsG
+    .append("g")
+    .attr("class", "x")
+    .attr("transform", `translate (0 , ${height + 62})`)
+    .call(xAxis);
+
+  buildingsG
+    .selectAll(".x text")
+    .attr("transform", `rotate (-40)`)
+    .attr("text-anchor", "end");
 };
 
 const drawBuildings = function(buildings) {
--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,5 @@
 const svgHeight = 400;
 const svgWidth = 400;
+
+const chartWidth = 600;
+const chartHeight = 400;
